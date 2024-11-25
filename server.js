3430c632a31875e0ab04cf8e2f7e78bfc3e1b49a
Maruka <emanuelle_carollina@hotmail.com>
2024-11-25 01:48:19 -0300

    rota inicial

--- server.js
+++ server.js
@@ -2,5 +2,9 @@ import express from "express"
 
 const app = express();
 app.listen(3000, () => {
-    console.log("Servidor escutando...")
+    console.log("Servidor escutando...");
+});
+
+app.get("/api", (req, res) => {
+    res.status(200).send("Rota inicial.");
 });
