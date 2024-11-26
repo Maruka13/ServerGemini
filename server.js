cb0748f9b71e59badc3c6e2c6d90d4836e5a5f61
Maruka <emanuelle_carollina@hotmail.com>
2024-11-26 10:07:15 -0300

    convert txt em json

--- server.js
+++ server.js
@@ -4,14 +4,31 @@ cost posts = [
     {
         descrição: "Foto teste",
         imagem: "http://placecats.com/millie/300/150",    //exemplo
+
+        descrição: "Foto teste2",
+        imagem: "http://placecats.com/millie/300/150",
+
+        descrição: "Foto teste3",
+        imagem: "http://placecats.com/millie/300/150",
+
+        descrição: "Foto teste4",
+        imagem: "http://placecats.com/millie/300/150",
+
+        descrição: "Foto teste5",
+        imagem: "http://placecats.com/millie/300/150",
+
+        descrição: "Foto teste6",
+        imagem: "http://placecats.com/millie/300/150",
     }
-]
+];
 
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("Rota inicial.");
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
 });
